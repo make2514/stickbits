9f2352282ff7bc64cd9e44fad0e46f165ee87345
Toan Nguyen <toan.k.nguyen.dev@gmail.com>
2022-03-31 00:48:43 +0300

    Display habits' names with data from Habits API response

--- app/apis/generics.js
+++ app/apis/generics.js
@@ -8,3 +8,14 @@ export function post(apiName, options) {
     body: JSON.stringify(options),
   }).then(res => res.json());
 }
+
+export function get(apiName, token) {
+  return fetch(`http://localhost:3001/api/${apiName}`, {
+    method: 'get',
+    headers: {
+      Authorization: `bearer ${token}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  }).then(res => res.json());
+}
--- app/components/HabitList.js
+++ app/components/HabitList.js
@@ -1,99 +1,87 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Grommet, Box, DataTable } from 'grommet';
+import { get } from '../apis/generics';
 
-export default () => (
-  <Grommet full>
-    <Box
-      fill="vertical"
-      overflow="auto"
-      align="center"
-      flex="grow"
-      pad="xsmall"
-      justify="start"
-      direction="column"
-      responsive
-      wrap
-    >
-      <DataTable
-        columns={[
-          {
-            header: '',
-            property: 'name',
-            primary: false,
-            render: datum => (
-              <Box>
-                <Link
-                  style={{ textDecoration: 'none' }}
-                  to="/singlehabitview/1"
-                >
-                  {datum.name}
-                </Link>
-              </Box>
-            ),
-          },
-          {
-            header: 'Tue',
-            property: 'day0',
-            primary: false,
-          },
-          {
-            property: 'day1',
-            header: 'Wed',
-            primary: false,
-          },
-          {
-            property: 'day2',
-            header: 'Thurs',
-            primary: false,
-          },
-          {
-            property: 'day3',
-            header: 'Fri',
-            primary: false,
-          },
-        ]}
-        data={[
-          {
-            name: 'Habit 1',
-            day0: 'x',
-            day1: 'x',
-            day2: 'x',
-            day3: 'x',
-          },
-          {
-            name: 'Habit 2',
-            day0: 'x',
-            day1: 'o',
-            day2: 'o',
-            day3: 'x',
-          },
-          {
-            name: 'Habit 3',
-            day0: 'x',
-            day1: 'x',
-            day2: 'o',
-            day3: 'x',
-          },
-          {
-            name: 'Habit 4',
-            day0: 'x',
-            day1: 'x',
-            day2: 'o',
-            day3: 'x',
-          },
-          {
-            name: 'Habit 5',
-            day0: 'x',
-            day1: 'x',
-            day2: 'x',
-            day3: 'x',
-          },
-        ]}
-        fill="horizontal"
-        replace={false}
-        sortable={false}
-      />
-    </Box>
-  </Grommet>
-);
+export default () => {
+  const [habits, setHabits] = useState([]);
+
+  useEffect(() => {
+    const mounted = true;
+    const token = localStorage.getItem('token');
+
+    get('habits', token)
+      .then(habitsData => {
+        if (mounted) {
+          setHabits(habitsData);
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  function transformHabitData(habitsData) {
+    return habitsData.map(habit => habit);
+  }
+
+  return (
+    <Grommet full>
+      <Box
+        fill="vertical"
+        overflow="auto"
+        align="center"
+        flex="grow"
+        pad="xsmall"
+        justify="start"
+        direction="column"
+        responsive
+        wrap
+      >
+        <DataTable
+          columns={[
+            {
+              header: '',
+              property: 'name',
+              primary: false,
+              render: datum => (
+                <Box>
+                  <Link
+                    style={{ textDecoration: 'none' }}
+                    to="/singlehabitview/1"
+                  >
+                    {datum.name}
+                  </Link>
+                </Box>
+              ),
+            },
+            /* {
+              header: 'Tue',
+              property: 'day0',
+              primary: false,
+            },
+            {
+              property: 'day1',
+              header: 'Wed',
+              primary: false,
+            },
+            {
+              property: 'day2',
+              header: 'Thurs',
+              primary: false,
+            },
+            {
+              property: 'day3',
+              header: 'Fri',
+              primary: false,
+            }, */
+          ]}
+          data={transformHabitData(habits)}
+          fill="horizontal"
+          replace={false}
+          sortable={false}
+        />
+      </Box>
+    </Grommet>
+  );
+};
